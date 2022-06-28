0805f93851b02c93b3603db50714da68c8bd6f09
valera <traberker901@gmail.com>
2022-06-28 10:18:43 +0300

    HW 5 finish

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,37 +1,16 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {NavLink} from "react-router-dom";
 import {PATH} from "./Routs";
 import s from "./pages/NavBar.module.css"
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
 
 function Header() {
-    const [hiddenNavBar, setHiddenNavBar] = useState<string>(`${s.navBarCollapsed}`)
-
-    const onClickHandler = () => {
-        if (hiddenNavBar === `${s.navBarCollapsed}`) {
-            setHiddenNavBar(`${s.navBarDefault} ${s.navLink}`)
-        } else {
-            setHiddenNavBar(`${s.navBarCollapsed}`)
-        }
-    }
-
-    let stateNavLink = (params: { isActive: boolean }) => {
-        let active = params.isActive
-        return {color: active ? "lightskyblue" : "blueviolet", textDecoration: 'none', marginRight: "10px"}
-    }
     return (
         <div className={s.wrapper}>
-            <div className={hiddenNavBar}>
-                <NavLink to={PATH.PRE_JUNIOR}
-                         style={stateNavLink}
-                >PreJunior
-                </NavLink>
-                <NavLink to={PATH.JUNIOR} style={stateNavLink}>Junior
-                </NavLink>
-                <NavLink to={PATH.JUNIOR_PLUS}
-                         style={stateNavLink}>Junior+ </NavLink>
-            </div>
-            <SuperButton onClick={onClickHandler}>Menu</SuperButton>
+            <NavLink to={PATH.PRE_JUNIOR} className={s.linkNavBar}>PreJunior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={s.linkNavBar}>Junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={s.linkNavBar}>Junior+ </NavLink>
+            <SuperButton className={s.buttonMenu}></SuperButton>
         </div>
     )
 }
