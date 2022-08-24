2b23a45e9522182915a3daaf12e73cbd4aa7473a
valera <traberker901@gmail.com>
2022-08-24 13:10:32 +0300

    Double range

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -6,24 +6,27 @@ function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+    const onChangeValue = (value: number) => {
+        setValue1(value)
+    }
+    const onChangeRange = (newValue: number | number[]) => {
+        let newVal = newValue as number[]
+        setValue2(newVal[1])
+        setValue1(newVal[0])
+    }
     return (
         <div>
             <hr/>
             homeworks 11
-
             {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
+                <SuperRange onChangeRange={onChangeValue} value={value1}/>
             </div>
 
             <div>
                 <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
+                <SuperDoubleRange max={100} min={0} onChangeRange={onChangeRange} value={[value1, value2]}/>
                 <span>{value2}</span>
             </div>
 
